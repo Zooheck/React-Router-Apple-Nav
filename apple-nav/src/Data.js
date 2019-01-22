f0dbdbee6ad0b41afb665de9b27cf2dab16c511c
Zooheck <davidscottflack@gmail.com>
2019-01-22 17:17:23 -0600

    Initialize JSON data

---
+++ apple-nav/src/Data.js
@@ -0,0 +1,39 @@
+export const NavItems = [
+    {name: '',},
+    {
+      name: 'Mac',
+      subHeadings: ['MacBook', 'MacBook Air', 'MacBook Pro', 'iMac', 'iMac Pro', 'Mac Pro', 'Mac mini', 'Accessories', 'Mojave','Compare']
+  },
+  {
+    name: "iPad",
+    subHeadings: ['iPad Pro New', 'iPad Pro 10.5', 'iPad 9.7"', 'iPad mini 4', 'iOS 12', 'Apple Pencil', 'Smart Keyboard', 'Accessories', 'Compare'] 
+  },
+    {
+      name: 'iPhone',
+      subHeadings: ['iPhone xs', 'iPhone Xr', 'iPhone 8', 'iPhone 7', 'iOS 12', 'AirPods', 'Accessories','Compare']
+  },
+  {
+    name: "Watch",
+    subHeadings: ['Apple Watch Series 4', 'Apple Watch Nike+', 'Apple Watch Hermes', 'Apple Watch Series 3', 'watchOS', 'Bands', 'Accessories', 'Compare']
+  },
+    {
+      name: 'TV',
+      subHeadings: ['Apple TV 4K', 'Apple TV', 'TV App', 'Accessories', 'Compare']
+  },
+    {
+      name: "Music",
+      subHeadings: ['Apple Music', 'iTunes', 'HomePod', 'AirPods', 'iPod Touch', 'Music Accessories', 'Gift Cards'] 
+    },
+    {
+      name: 'Support',
+      subHeadings: null
+  },
+    {
+      name: '🔍',
+    subHeadings: null
+    },
+    {
+      name: 'shop',
+    subHeadings: null
+  }
+  ];
---
+++ apple-nav/src/components/NavWrapper.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavWrapper = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NavWrapper
