ec2714adedb620576a50cf367c9ab4d7ec0cbe61
Zooheck <davidscottflack@gmail.com>
2019-01-22 17:23:45 -0600

    Add browser router

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavItems from './Data'
+import NavWrapper from './components/NavWrapper';
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      navItems: []
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      navItems: NavItems
+    })
+  }
   render() {
     return (
       <div className="App">
-      
+        <NavWrapper />
       </div>
     );
   }
